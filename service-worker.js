2980121afab1d1cb9d25c45f694d421d6fae311f
Eleven <idiotalex@163.com>
2023-02-23 17:11:26 +0800

    rm files

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-function logURL(requestDetails) {
-  console.log(`Loading: ${requestDetails.url}`);
-}
-
-chrome.webRequest.onBeforeRequest.addListener(
-  logURL,
-  {urls: ["http://*/*", "https://*/*"]}
-);
