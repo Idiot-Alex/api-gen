170b8552b18ee24eccfc3d195755a2b5ac322ce2
Eleven <idiotalex@163.com>
2023-02-24 18:58:08 +0800

    rm nuxt

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtConfig } from "nuxt/config";
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: [
-    '@nuxt/devtools',
-  ],
-})
