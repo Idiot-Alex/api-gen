17b5e69c8b3ce73709b64291b2fa6076efea76ad
Eleven <idiotalex@163.com>
2023-02-24 18:23:06 +0800

    init nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 const route = useRoute()
+const runPlaywright = async () => {
+  // const browser = await chromium.launch({
+  //   args: ["--remote-debugging-port=9222"],
+  //   headless: false,
+  // });
+}
 </script>
 
 <template>
@@ -7,5 +13,7 @@ const route = useRoute()
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <button @click="runPlaywright">clicke me</button>
+
   </div>
 </template>
--- playwright.js
+++ playwright.js
@@ -2,13 +2,32 @@
 
 const { chromium } = require("playwright");
 
-(async () => {
+const app = async() => {
   const browser = await chromium.launch({
     args: ["--remote-debugging-port=9222"],
     headless: false,
   });
 
-  const page = await browser.newPage();
+  // const page = await browser.newPage();
 
   // http.get("http://localhost:9999");
-})();
+}
+
+const cdp = async () => {
+  const browser = await chromium.connectOverCDP('http://127.0.0.1:9222/');
+  // let ctx = browser.contexts()[0];
+  // let page = await ctx.newPage();
+  
+  const page = await browser.newPage();
+  
+  page.on("request", (request) => console.log(">>", request.method(), request.url()))
+  page.on("response", (response) => console.log("<<", response.status(), response.url()))
+  
+  await page.goto("https://www.google.com/")
+  
+}
+
+(async() => {
+  await app()
+  await cdp()
+})()
