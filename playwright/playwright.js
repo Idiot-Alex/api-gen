85a5377d5693edc13e7883f08ec96496893a329f
Eleven <idiotalex@163.com>
2023-02-27 15:22:01 +0800

    use browserContext listening new page

--- playwright/playwright.js
+++ playwright/playwright.js
@@ -15,87 +15,95 @@ const app = async() => {
 const cdp = async () => {
   const browser = await chromium.connectOverCDP('http://127.0.0.1:9222/');
   
+ 
+
   const page = await browser.newPage();
 
 
-  browser.contexts().forEach(browserContext => {
-    browserContext.pages().forEach(page => {
-      page.on('requestfailed', async(request) => {
-        const response = await request.response()
-        if (!response) {
-          return;
-        }
+  // browser.contexts().forEach(browserContext => {
+  //   browserContext.pages().forEach(page => {
+  //     page.on('requestfailed', async(request) => {
+  //       const response = await request.response()
+  //       if (!response) {
+  //         return;
+  //       }
     
-        const data = {
-          url: request.url(),
-          method: request.method(),
-          request: {
-            resourceType: request.resourceType(),
-            // headers: request.allHeaders(),
-            postData: request.postData(),
-            failed: true,
-            errorText: request.failure().errorText
-          },
-          response: {
-            // headers: response.allHeaders(),
-            // text: text,
-            status: response.status(),
-            statusText: response.statusText()
-          }
-        }
-        upload(data).then((res) => {
-          console.log(res)
-        })
-      })
-      page.on('requestfinished', async(request) => {
-        const response = await request.response()
-        let text = ''
+  //       const data = {
+  //         url: request.url(),
+  //         method: request.method(),
+  //         request: {
+  //           resourceType: request.resourceType(),
+  //           // headers: request.allHeaders(),
+  //           postData: request.postData(),
+  //           failed: true,
+  //           errorText: request.failure().errorText
+  //         },
+  //         response: {
+  //           // headers: response.allHeaders(),
+  //           // text: text,
+  //           status: response.status(),
+  //           statusText: response.statusText()
+  //         }
+  //       }
+  //       upload(data).then((res) => {
+  //         console.log(res)
+  //       })
+  //     })
+  //     page.on('requestfinished', async(request) => {
+  //       const response = await request.response()
+  //       let text = ''
 
-        if (response.status() !== 302) {
-          const contentType = response.headers()['content-type'];
-          console.log(contentType)
-          if (contentType && (contentType.includes('text/') || contentType === 'application/json')) {
-            if (response.ok()) {
-              try {
-                text = await response.text();
-              } catch (error) {
-                console.log('error: ', error)
-              }
-              console.log(text);  
-            }
-          }
-        }
+  //       if (response.status() !== 302) {
+  //         const contentType = response.headers()['content-type'];
+  //         console.log(contentType)
+  //         if (contentType && (contentType.includes('text/') || contentType === 'application/json')) {
+  //           if (response.ok()) {
+  //             try {
+  //               text = await response.text();
+  //             } catch (error) {
+  //               console.log('error: ', error)
+  //             }
+  //             console.log(text);  
+  //           }
+  //         }
+  //       }
 
-        // console.log(text)
+  //       // console.log(text)
     
-        const data = {
-          url: request.url(),
-          method: request.method(),
-          request: {
-            resourceType: request.resourceType(),
-            headers: request.headers(),
-            postData: request.postData(),
-            failed: false
-          },
-          response: {
-            headers: response.headers(),
-            text: text,
-            status: response.status(),
-            statusText: response.statusText()
-          }
-        }
-        upload(data).then((res) => {
-          console.log(res)
-        })
-      })
-    })
-  });
+  //       const data = {
+  //         url: request.url(),
+  //         method: request.method(),
+  //         request: {
+  //           resourceType: request.resourceType(),
+  //           headers: request.headers(),
+  //           postData: request.postData(),
+  //           failed: false
+  //         },
+  //         response: {
+  //           headers: response.headers(),
+  //           text: text,
+  //           status: response.status(),
+  //           statusText: response.statusText()
+  //         }
+  //       }
+  //       upload(data).then((res) => {
+  //         console.log(res)
+  //       })
+  //     })
+  //   })
+  // });
   
   // page.on("request", (request) => console.log(">>", request.method(), request.url()))
   // page.on("response", (response) => console.log("<<", response.status(), response.url()))
 
+
+  browser.contexts().forEach(browserContext => {
+    browserContext.on('request', (request) => console.log(">>", request.method(), request.url()))
+  })
   
   await page.goto("https://www.google.com/")
+
+
 }
 
 (async() => {
