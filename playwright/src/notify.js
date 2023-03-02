6e16fc476cb9596e858524882c76b6e23e03eab5
Eleven <idiotalex@163.com>
2023-03-03 00:49:05 +0800

    update notify message

--- playwright/src/notify.js
+++ playwright/src/notify.js
@@ -1,8 +1,10 @@
 import notifier from 'node-notifier'
 
 export function notifyError(message) {
+  const errorMsg = message.split('\n')[0]?.split(':')[1]
+  const errUrl = message.split('\n')[2]
   notifier.notify({
     title: 'Error',
-    message: message
+    message: `${errorMsg} \n ${errUrl}`
   });
 }
