68b54b77ab966296a3486450e498b3ec46d4e69f
Eleven <idiotalex@163.com>
2023-04-06 11:35:51 +0800

    init docs for docute

---
+++ docs/config-nav.js
@@ -0,0 +1,30 @@
+const _nav = [
+  {
+    title: 'Home',
+    link: '/'
+  },
+  // A dropdown menu
+  {
+    title: '语言',
+    children: [
+      {
+        title: '2',
+        link: '/'
+      }
+    ]
+  },
+  // A dropdown menu
+  {
+    title: 'Community',
+    children: [
+      {
+        title: 'Spectrum',
+        link: 'https://spectrum.chat/your-community'
+      },
+      {
+        title: 'Discord',
+        link: 'https://discord.app/your-discord-server'
+      }
+    ]
+  }
+]
---
+++ docs/config-sidebar.js
@@ -0,0 +1,21 @@
+const _sidebar = [
+  // A sidebar item, with child links
+  {
+    title: '教程',
+    children: [
+    {
+      title: '快速开始',
+      link: '/a/start'
+    },
+    {
+      title: '安装',
+      link: '/install'
+    }
+    ]
+  },
+  // An external link
+  {
+    title: 'GitHub',
+    link: 'https://github.com/egoist/docute'
+  }
+]
