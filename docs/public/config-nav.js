7c17c81763890309141b8d63eccb9e0ca9078dfc
Eleven <idiotalex@163.com>
2023-04-06 16:32:57 +0800

    add docute docs

---
+++ docs/public/config-nav.js
@@ -0,0 +1,20 @@
+const _nav = [
+  {
+    title: 'Home',
+    link: '/'
+  },
+  // A dropdown menu
+  {
+    title: '版本',
+    children: [
+      {
+        title: 'v1',
+        link: '/v1/'
+      },
+      // {
+      //   title: 'v2',
+      //   link: '/v2/'
+      // }, 
+    ]
+  }
+]
---
+++ docs/public/config-sidebar.js
@@ -0,0 +1,17 @@
+const _sidebar = [
+  // A sidebar item, with child links
+  {
+    title: '使用说明',
+    children: [
+      {
+        title: '快速开始',
+        link: '/v1/start/'
+      }
+    ]
+  },
+  // An external link
+  {
+    title: 'GitHub',
+    link: 'https://github.com/Idiot-Alex/api-gen'
+  }
+]
