ffa8aed3e8743fdc4ddc85c7b3ecd547c7e77c0e
Eleven <idiotalex@163.com>
2023-05-11 00:16:07 +0800

    add api-doc-list

--- web-admin/src/utils/menu.ts
+++ web-admin/src/utils/menu.ts
@@ -2,6 +2,7 @@ const menus = [
   { path: '/guide', title: '使用说明', icon: 'Location' },
   { path: '/dashboard', title: '数据面板', icon: 'Menu' },
   { path: '/api-log-list', title: 'Api 列表', icon: 'Document' },
+  { path: '/api-doc-list', title: 'Api 文档', icon: 'Book' },
   { path: '/sys-config-list', title: '系统配置', icon: 'Setting' },
 ]
 
