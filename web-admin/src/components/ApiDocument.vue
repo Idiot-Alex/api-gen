8dcb0d8e7ea07af1e075c8140684dfa5225c122a
Eleven <idiotalex@163.com>
2023-05-21 23:57:03 +0800

    update api document

--- web-admin/src/components/ApiDocument.vue
+++ web-admin/src/components/ApiDocument.vue
@@ -4,6 +4,17 @@
 
 <template>
   <el-row>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>Card name</span>
+          <el-button class="button" text>Operation button</el-button>
+        </div>
+      </template>
+      <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+    </el-card>
+  </el-row>
+  <el-row>
     <el-col :span="6">
       <el-menu
         class="el-menu-vertical-demo">
