55d10238d4b9e9d3707daf51bcdbe4117740817b
hotstrip <idiotalex@163.com>
2023-05-23 15:14:22 +0800

    add api-doc api

---
+++ web-admin/src/api/api-doc.ts
@@ -0,0 +1,13 @@
+import axios from './config'
+import { MyAxiosResponse } from '~/utils/types'
+
+/**
+ * create a new doc
+ * @param params 
+ * @returns 
+ */
+export function create(params: Object): Promise<MyAxiosResponse> {
+  return axios.post('/w/api-doc/create', {
+    ...params
+  })
+}
